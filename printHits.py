a13932a150ec34fa417b717dadcfddc254325b2c
Dominic Smith <dosmith@cern.ch>
2017-08-08 19:07:07 +0200

    Updates to run over more than one city

--- printHits.py
+++ printHits.py
@@ -10,46 +10,55 @@ import matplotlib.pyplot as plt
 
 ##_______________________________________________________||
 parser = argparse.ArgumentParser()
-parser.add_argument("-l", "--location", type=str, help="Location")
-args = parser.parse_args()
+parser.add_argument("-l", "--location", nargs ="*", type=str, help="Location")
 
 ##_______________________________________________________||
-if not args.location:
-    print "Please provide an input location"
-page = 'https://{0}.craigslist.org/search/bia'.format(args.location)
-raw = requests.get(page).text
-soup = BeautifulSoup(raw,'lxml')
-
-##_______________________________________________________||
-def meanPrice(data, location):
+def meanPrice(data, locations):
     # Convert prices from strings to ints
-    prices = [int(price) for price in data.keys()]
+    for location in locations:
+        prices = [int(price) for price in data[location].keys()]
 
-    # Convert prices list to numpy array
-    prices_array = np.asarray(prices)
-    sns_plot = sns.distplot(prices_array,kde=False, color="b",label=location)
-    plt.xlabel('bike price (euros)')
-    plt.ylabel('mean occurances')
-    plt.legend()
-    plt.savefig('prices.png')
-
+        # Convert prices list to numpy array
+        prices_array = np.asarray(prices)
+        sns_plot = sns.distplot(prices_array,kde=False,label=location)
+        plt.xlabel('bike price (euros)')
+        plt.ylabel('mean occurances')
+        plt.legend()
+        plt.savefig('prices_{0}.png'.format(location))
+    plt.savefig('prices_comp.png')
+    
 ##_______________________________________________________||
 def main():
 
-    title = soup.title.text
-    results =  soup.find_all('li',class_='result-row')
-    data = {}
+    args = parser.parse_args()
 
-    for result in results:
-        key = result.a.get_text().encode('ascii', 'ignore').replace('\n','')
-        if not key: continue
-        data[key] = {}
-        data[key]['link'] = result.a['href']
-        data[key]['description'] = result.find('a',class_='result-title hdrlnk').text
+    if not args.location:
+        print "Please provide an input location"
+
+    data = {}
         
+    for location in args.location:
+        page = 'https://{0}.craigslist.org/search/bia'.format(location)
+        raw = requests.get(page).text
+        soup = BeautifulSoup(raw,'lxml')
+
+        title = soup.title.text
+        results =  soup.find_all('li',class_='result-row')
+        data[location] = {}
+        
+        for result in results:
+            key = result.a.get_text().encode('ascii', 'ignore').replace('\n','')
+            if not key: continue
+            data[location][key] = {}
+            data[location][key]['link'] = result.a['href']
+            data[location][key]['description'] = result.find('a',class_='result-title hdrlnk').text
+
+        df = pd.DataFrame(data[location].items(),columns=['Price','Info'])
+        df['Price'] = pd.to_numeric(df['Price']).fillna(value=0)
+
     meanPrice(data, args.location)
-    df = pd.DataFrame(data.items(),columns=['Price','Info'])
-    df['Price'] = pd.to_numeric(df['Price']).fillna(value=0)
+    print data
+
     return data
 
 if __name__=='__main__':
